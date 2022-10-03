f84599295d7eb02d25ce8f95312788366151781a
Since02Prog <sabinojers@gmail.com>
2022-10-04 00:51:38 +0800

    first commit

---
+++ VSCodeforJava/javaprojects/index.java
@@ -0,0 +1,12 @@
+public class index
+{
+     public static void main(String[] args) 
+    {
+        System.out.println("NASA HACKING 1%");
+        System.out.println("NASA HACKING 20%");
+        System.out.println("NASA HACKING 40%");
+        System.out.println("NASA HACKING 60%");
+        System.out.println("NASA HACKING 80%");
+        System.out.println("NASA HACKING 100%");
+    }
+}
